4eca3a88c36c585259651c8d7a5dcb0dc171761a
ezequielh <>
2022-12-27 21:09:33 -0800

    bookShow now allows users to delete books

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './index.css';
 import { useState } from "react";
 import BookCreate from "./compnents/BookCreate";
+import BookList from './compnents/BookList';
 function App(){
     const [books, setBooks]= useState([]);
 
     const createBook = (title) => {
-        const updatedBooks = [...books,  {id:123, title}];
+        const updatedBooks = [...books,  {
+            id: Math.round(Math.random()*9999), title}];
+        setBooks(updatedBooks);
+    };
+
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((books)=> {
+            return books.id !== id;
+        });
         setBooks(updatedBooks);
     };
     
     return (
-    <div> 
-    <BookCreate onSubmit = {createBook}/>
+    <div className='app'> 
+    <BookList books = {books} onDelete = {deleteBookById}/>
+    <BookCreate onCreate= {createBook}/>
     </div>
     );
 
--- src/compnents/BookList.js
+++ src/compnents/BookList.js
@@ -1,5 +1,10 @@
-function BookList() {
+import BookShow from "./BookShow";
 
-    return <div>BookList</div>
+function BookList({books, onDelete}) {
+    const renderedBooks = books.map((book) => {
+        return <BookShow onDelete = {onDelete} key ={book.id} book={book}/>;
+    });
+
+    return <div className="book-list">{renderedBooks}</div>
 }
 export default BookList
